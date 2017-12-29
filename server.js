ac98890c387ab3ef99cb6ab908d5fee17730035d
ratnani1996 <himanshuratnani1996@gmail.com>
2017-12-29 14:02:16 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 app.use(morgan('dev'))
-
+app.use(cookieParser());
 
 app.get('/',(req, res)=>{
     res.send(`This is a get request to ${req.url}`)
